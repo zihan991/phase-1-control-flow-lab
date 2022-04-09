3b906a14e3dd4090513d4e514af5826520abcac2
mckinseyleckenby <leckenbym11@gmail.com>
2022-04-08 19:00:18 -0500

    commit tree b9a2b513b88207c6c9221f527781e2a8fefd6ef8

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!'
+  } else if (distance >= 400 && distance <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (distance >= 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else (distance > 2500)
+  return "No can do."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return (city === "NYC") ? "Ok, sounds good." : "No go."
+}
+console.log(ternaryCheckCity("SEA"))
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return ("Thank you.");
+    default:
+      return ("Bye.")
+  }
 }
