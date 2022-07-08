414581b8352c6601c4e95189264f1c2c94d4de52
ittehadzihan <zihan991@gmail.com>
2022-07-08 00:25:59 -0400

    DONE

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+    return 'This one is on me!'
+  }else if(distance > 400 && distance < 2000){
+    return 'That will be twenty bucks.'
+  }else if(distance > 2000 && distance < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else{
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === 'NYC'? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch(tip){
+    case tip ='generous':
+      return('Thank you so much.');
+    break;
+    case tip = 'not as generous':
+      return ('Thank you.')
+    break;
+    default:
+      return'Bye.'
+
+  }}
